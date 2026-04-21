9f561471ec25c4399151a1b8e693da276367e27c
Jesús Ramírez <jesusramirez1627@gmail.com>
2026-04-21 19:16:31 +0200

    feat: implementado CRUD de usuarios y roles (DEV-60)

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from app.routes import detectar, auth  # Esto está bien
 from app.database import engine, Base  # Esto está bien
+from app.routes import auth, users
 
 # Esto crea las tablas nuevas si no existen todavía
 Base.metadata.create_all(bind=engine)
@@ -9,6 +10,7 @@ app = FastAPI(title="StyleLens API", version="1.0")
 app.include_router(detectar.router, prefix="/api/v1", tags=["detectar"])
 # TU RUTA DE LOGIN (Tarea DEV-59)
 app.include_router(auth.router, prefix="/api/v1/auth", tags=["Autenticación"])
+app.include_router(users.router)
 
 
 @app.get("/")
